c814a1956c2b4c52ae4b0bbf3446a4ffed6189c5
Rashid H Armand <rashidharmand@gmail.com>
2019-01-25 11:01:38 -0500

    added actions/action-creators

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInitialData } from '../utils/api';
+import { setAuthedUser } from './authedUser';
+import { receiveTweets } from './tweets';
+import { receiveUsers } from './users';
+
+const AUTHED_ID = 'dan_abramov';
+
+export default function handleGetInitialData() {
+  return (dispatch) => {
+    return getInitialData
+      .then(([ users, tweets ]) => {
+        dispatch(receiveUsers(users));
+        dispatch(receiveTweets(tweets));
+        dispatch(setAuthedUser(AUTHED_ID));
+      })
+  }
+}

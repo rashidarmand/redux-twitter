9cd7328f79e6a7b91c26fabd6d9950544dd730d4
Rashid H Armand <rashidharmand@gmail.com>
2019-01-25 22:07:31 -0500

    finished tweet ui

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { formatDate } from '../utils/helpers';
+import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti'
 
-class Tweet extends Component {
+const Tweet = (props) => {
+  const { text, author, authorAvatar, timestamp, liked, likes, replies, replyingTo } = props;
 
-  render() {
-    const { text, author, authorAvatar, timestamp, likes, replies, replyingTo } = this.props;
-
-    return (
-      <div className='tweet'>
-        <img className='avatar' src={ authorAvatar } alt={ author } />
-        <div className="tweet-info">
-          <div className='author'>{ author }</div>
-          <div className='timestamp'>{ timestamp }</div>
-          {replyingTo !== null && <div className='replying-to'>Replying To @{ replyingTo }</div>}
-          <p>{ text }</p>
+  return (
+    <div className='tweet'>
+      <img className='avatar' src={ authorAvatar } alt={ author } />
+      <div className="tweet-info">
+        <div className='author'>{ author }</div>
+        <div className='timestamp'>{ timestamp }</div>
+        {replyingTo !== null && <div className='replying-to'>Replying To @{ replyingTo }</div>}
+        <p>{ text }</p>
+        <div className="tweet-icons">
+          <TiArrowBackOutline className='tweet-icon' /> <span className='buffer' /> { replies > 0 ? replies : ' ' }
+          <span className='spacer' />
+          {liked 
+            ? <TiHeartFullOutline style={{ color: 'rgb(224, 36, 94)' }} className='tweet-icon' /> 
+            : <TiHeartOutline className='tweet-icon' />} 
+          <span className='buffer' /> { likes > 0 ? likes : ' ' }
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+
 }
 
-const mapStateToProps = ({ users, tweets }, { id }) => {
+const mapStateToProps = ({ users, tweets, authedUser }, { id }) => {
   const soloTweet = tweets[id];
   const { text, author, timestamp, likes, replies, replyingTo } = soloTweet;
 
@@ -30,6 +37,7 @@ const mapStateToProps = ({ users, tweets }, { id }) => {
     author: users[author].name,
     authorAvatar: users[author].avatarURL,
     timestamp: formatDate(timestamp),
+    liked: likes.includes(authedUser),
     likes: likes.length,
     replies: replies.length,
     replyingTo: replyingTo === null ? null : tweets[replyingTo].author
